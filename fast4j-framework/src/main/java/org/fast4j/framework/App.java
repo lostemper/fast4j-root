20ed2966e651118b4e75283b9dd53e56e74b0305
richard <lostemper@gmail.com>
2017-03-21 16:54:54 +0800

    init 20170321

---
+++ fast4j-framework/src/main/java/org/fast4j/framework/App.java
@@ -0,0 +1,14 @@
+package org.fast4j.framework;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        //这个需要实现什么功能
+    }
+}
